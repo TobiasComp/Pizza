cae98eef4bd9696d46cefaacc0762a3275892f66
TobiasComp <tuvia1234@gmail.com>
2019-11-14 18:15:37 +0200

    pizza party

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './comps/general/header/header.component';
 import { FooterComponent } from './comps/general/footer/footer.component';
 import { NavComponent } from './comps/general/nav/nav.component';
+import { PaginatorComponent } from './comps/general/paginator/paginator.component';
+import { PizzaComponent } from './comps/panels/pizza/pizza.component';
+import { SaladComponent } from './comps/panels/salad/salad.component';
+import { DrinkComponent } from './comps/panels/drink/drink.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    PaginatorComponent,
+    PizzaComponent,
+    SaladComponent,
+    DrinkComponent
   ],
   imports: [
     BrowserModule
